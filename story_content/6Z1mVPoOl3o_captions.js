fdcb4f379513321a808603481512e43850f5880e
ximenacastano <ximenacastano@gmail.com>
2026-01-15 11:48:35 -0500

    Delete story_content directory

--- story_content/6Z1mVPoOl3o_captions.js
+++
@@ -1,1 +0,0 @@
-window.globalProvideData('caption', '{"data":"WEBVTT%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%203.98.34263.0%0D%0A%0D%0A00:00:00.150%20--%3E%2000:00:03.370%0D%0AFelicitaciones!%0D%0AHaz%20Finalizado%20el%20m%C3%B3dulo!%0D%0A%0D%0A"}')
